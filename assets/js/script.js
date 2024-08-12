8d64f7a3f205dce94c3976ae58fb9dd43e707c96
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-08-12 14:23:30 -0600

    adjusted local storage and fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,54 @@ const cityName = document.getElementById('cityName');
 let weatherSearchHistory = JSON.parse(localStorage.getItem('weatherSearchHistory')) || [];
 
 function savedCities() {
-    let savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
-    return savedCities;
+    let weatherSearchHistory = JSON.parse(localStorage.getItem('weatherSearchHistory')) || [];
+    return weatherSearchHistory;
+}
+
+function saveStorage {
+    localStorage.setItem('weatherSearchHistory', JSON.stringify(weatherSearchHistory));
 }
 
 //event listener on form submission
 citySearch.addEventListener("click", (event) => {
     savedCities();
     event.preventDefault();
+    getLocation;
 })
 
+//fetch city latitude longtitude
+function getLocation(city) {
+    const userInput = document.getElementById('cityName');
+    let search;
+    if (!city) {
+        search = userInput.value;
+    } else {
+        search = city;
+    }
 
-const geoApi = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
-console.log(geoApi);
+const geoApi = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`;
+console.log(geoApi)
 
-// const weatherAPI `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+fetch(geoApi)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    getWeather(data);
+    getForecast(data);
+    userInput.value = '';
+})
+}
+
+function getWeather(data) {
+    savedCities();
+    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}&units=imperial`;
+
+    fetch(weatherAPI)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(weather) {
+        displayWeather(weather);
+    })
+}
